bbce2398b2ec516bb6bdedee9fc60e5d91bd28d9
Ajarek <ajarek@poczta.onet.pl>
2025-08-30 00:49:22 +0200

    added convex

---
+++ app/notes/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notes = () => {
+  return (
+    <div className="min-h-screen w-full max-w-7xl mx-auto flex flex-col items-center justify-between p-4 md:p-8 lg:p-16">
+      <h1 className='text-4xl font-bold'>Notes</h1>
+    </div>
+  )
+}
+
+export default Notes
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const Home = ()=> {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Click</Button>
+    <div className="min-h-screen w-full max-w-2xl mx-auto flex flex-col items-center justify-between py-4 md:py-8 lg:py-16">
+      <div className="relative w-30 h-30">
+        <Image src='/logo.webp' alt='logo' fill className='object-cover'/>
+      </div>
+      <h1 className='text-4xl sm:text-5xl lg:text-6xl font-bold tracking-tight'>Smart Notes</h1>
+      <p className='text-lg sm:text-xl text-muted-foreground max-w-2xl mx-auto leading-relaxed' >A simple note-taking app with AI chatbot integration. Ask the chatbot anything about your notes to retrieve and summarize that information.</p>
+      <Button asChild className="px-8 py-3 text-lg">
+        <Link href='/notes'>Get Started</Link> 
+      </Button>
+      <div className="pt-8 text-sm text-muted-foreground">
+        <p>Built with Convex and the Vercel AI SDK</p>
+      </div>
     </div>
   )
 }
+export default Home
