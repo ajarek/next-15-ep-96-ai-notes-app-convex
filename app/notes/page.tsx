abb3e30fcc869d1c5d28a38c3954492f4a7d4a88
Ajarek <ajarek@poczta.onet.pl>
2025-09-02 23:59:03 +0200

    added chat ai

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,3 +1,4 @@
+import { AIChatButton } from '@/components/AIChatButton'
 import { CreateNoteButton } from '@/components/CreateNote'
 import Navbar from '@/components/Navbar'
 import { NotesPage } from '@/components/NotePage'
@@ -12,7 +13,7 @@ const Notes = () => {
       <section className='w-full flex items-center justify-between px-4 md:px-8 lg:px-16'>
       <h1 className='text-2xl font-bold'>My Notes</h1>
       <div className='flex items-center gap-4'>
-        <Button className='flex items-center gap-2' variant={'outline'}><Bot /> Ask AI</Button>
+        <AIChatButton/>
         <CreateNoteButton/>
       </div>
      
